7670f72448e683a5f8142a89b324df0221b1b727
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-04-23 23:22:08 +0530

    Update userSignUp.js

--- backend/controller/userSignUp.js
+++ backend/controller/userSignUp.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/userModel")
+const bcrypt = require('bcryptjs')
 
 async function userSignUpController(req, res) {
     try {
@@ -13,19 +14,41 @@ async function userSignUpController(req, res) {
             throw new Error("Name Provide")
         }
 
+        const salt = bcrypt.genSaltSync(10)
+        const hashPassword = await bcrypt.hashSync(password, salt)
+
         /* const userData = new userModel({
             email,
             password,
             name,
         })*/
 
+        if (!hashPassword) {
+            throw new Error("Password Field Error")
+        }
+
+        const payload = {
+            ...req.body,
+            password: hashPassword
+        }
+
         const userData = new userModel(req.body)
+        const saveUser = userData.save()
+
+        res.status(201).json({
+            data: saveUser,
+            success: true,
+            error: false,
+            message: "User Created OK!!"
+        })
 
     } catch (err) {
-        req.json({
+        res.json({
             message: err,
             error: true,
             success: false,
         })
     }
 }
+
+module.exports = userSignUpController
