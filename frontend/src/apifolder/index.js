0686447ecb4b39b1f742543bc790bb2e39a07374
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-04-25 00:54:40 +0530

    Create index.js

---
+++ frontend/src/apifolder/index.js
@@ -0,0 +1,10 @@
+const backendDomin = "http://localhost:8080"
+
+const SummaryApi ={
+    signUP : {
+        url : `${backendDomin}/api/signup`,
+        method : "post"
+    }
+}
+
+export default SummaryApi 
