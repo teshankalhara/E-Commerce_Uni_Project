7c77013454857e00050dc0dc4ec00ec22e6c36db
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-04-25 00:54:42 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import loginIcons from '../assets/signin.gif'
 import { FaEye } from 'react-icons/fa'
 import { FaEyeSlash } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 import imageTobase64 from '../helpers/imageTobase64'
-
+import SummaryApi from '../apifolder'
+import { toast } from 'react-toastify';
 
 const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false)
@@ -16,6 +17,8 @@ const SignUp = () => {
         confirmpassword: "",
         profilePic: ""
     })
+
+    const navigate = useNavigate()
 
     const handleOnChange = (e) => {
         const { name, value } = e.target
@@ -31,22 +34,46 @@ const SignUp = () => {
         const file = e.target.files[0]
 
         const imagePic = await imageTobase64(file)
-        
+
         // console.log("imagePic ", imagePic)
 
-        setData((prev)=>{
-            return{
+        setData((prev) => {
+            return {
                 ...prev,
-                profilePic:imagePic
+                profilePic: imagePic
             }
         })
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-    }
-
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        if (data.password === data.confirmpassword) {
+            //console.log('backendurl:',SummaryApi.signUP.url);
+            const dataResponse = await fetch(SummaryApi.signUP.url,{
+                method : SummaryApi.signUP.method,
+                headers : {
+                    "content-type" : "application/json"
+                },
+                body : JSON.stringify(data)
+              })
+        
+              const dataApi = await dataResponse.json()
+    
+              if(dataApi.success){
+                toast.success(dataApi.message) //get msg for user create ok
+                navigate("/login")
+              }
+    
+              if(dataApi.error){
+                toast.error(dataApi.message) //get msg for user create not ok
+              }
+        
+          }else{
+            toast.error("Please check password and confirm password") //get msg for user check password
+          }
+    };
+    
     console.log("data-log", { data })
     return (
         <section id='signup'>
@@ -54,7 +81,7 @@ const SignUp = () => {
                 <div className='bg-white p-5 w-full max-w-sm mx-auto roun'>
                     <div className='w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
                         <div>
-                            <img src={data.profilePic||loginIcons} alt='login icons' />
+                            <img src={data.profilePic || loginIcons} alt='login icons' />
                         </div>
                         <form>
                             <label>
@@ -70,37 +97,37 @@ const SignUp = () => {
                         <div className='grid'>
                             <label>Name : </label>
                             <div className='bg-slate-200 p-2'>
-                                <input 
-                                type='text' 
-                                placeholder='Enter Your Name' 
-                                name='name' value={data.name} 
-                                required
-                                onChange={handleOnChange} 
-                                className='w-full h-full outline-none bg-transparent'
+                                <input
+                                    type='text'
+                                    placeholder='Enter Your Name'
+                                    name='name' value={data.name}
+                                    required
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'
                                 />
                             </div>
                         </div>
                         <div className='grid'>
                             <label>Email : </label>
                             <div className='bg-slate-200 p-2'>
-                                <input type='email' 
-                                placeholder='Enter Email' 
-                                name='email' value={data.email} 
-                                required
-                                onChange={handleOnChange} 
-                                className='w-full h-full outline-none bg-transparent'
+                                <input type='email'
+                                    placeholder='Enter Email'
+                                    name='email' value={data.email}
+                                    required
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'
                                 />
                             </div>
                         </div>
                         <div>
                             <label>Password : </label>
                             <div className='bg-slate-200 p-2 flex'>
-                                <input 
-                                type={showPassword ? "text" : "password"} 
-                                name='password' value={data.password} 
-                                required
-                                onChange={handleOnChange} placeholder='Enter Password' 
-                                className='w-full h-full outline-none bg-transparent' 
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    name='password' value={data.password}
+                                    required
+                                    onChange={handleOnChange} placeholder='Enter Password'
+                                    className='w-full h-full outline-none bg-transparent'
                                 />
 
                                 <div className='cursor-pointer text-xl' onClick={() => setShowPassword((prev) => !prev)}>
@@ -121,12 +148,12 @@ const SignUp = () => {
                         <div>
                             <label>Confirm Password : </label>
                             <div className='bg-slate-200 p-2 flex'>
-                                <input 
-                                type={showConfirmPassword ? "text" : "password"} 
-                                name='confirmpassword' value={data.confirmpassword} 
-                                required
-                                onChange={handleOnChange} placeholder='Confirm Password' 
-                                className='w-full h-full outline-none bg-transparent'
+                                <input
+                                    type={showConfirmPassword ? "text" : "password"}
+                                    name='confirmpassword' value={data.confirmpassword}
+                                    required
+                                    onChange={handleOnChange} placeholder='Confirm Password'
+                                    className='w-full h-full outline-none bg-transparent'
                                 />
                                 <div className='cursor-pointer text-xl' onClick={() => setShowConfirmPassword((prev) => !prev)}>
                                     <span>
