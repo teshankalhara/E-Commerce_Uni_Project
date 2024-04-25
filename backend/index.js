613eee2fd3c621b0981131d2ac9799002bd33408
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-04-26 00:30:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes/index')
@@ -7,6 +8,7 @@ const router = require('./routes/index')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api", router)
 
