746764b2ad03a77667b0610e5c9726852516dcc7
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-04-30 01:05:19 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,9 @@ import loginIcons from '../assets/signin.gif'
 import { FaEye } from 'react-icons/fa'
 import { FaEyeSlash } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import imageTobase64 from '../helpers/imageTobase64'
+import SummaryApi from '../api'
+import { toast } from 'react-toastify';
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false)
@@ -21,42 +24,60 @@ const Login = () => {
         })
     }
 
-    const handleSubmit=(e)=>{
-       e.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const dataResponse = await fetch(SummaryApi.signIN.url, {
+            method: SummaryApi.signIN.method,
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        const dataApi = await dataResponse.json()
+
+        if (dataApi.success) {
+            toast.success(dataApi.message)
+        }
+        
+        if (dataApi.error) {
+            toast.error(dataApi.message)
+        }
+
 
     }
 
-    console.log("data login",data)
+    console.log("data login", data)
 
     return (
         <section id='login'>
             <div className='mx-auto container p-4'>
                 <div className='bg-white p-5 w-full max-w-sm mx-auto roun'>
                     <div className='w-20 h-20 mx-auto'>
-                        <img src={loginIcons} alt='login icons'/>
+                        <img src={loginIcons} alt='login icons' />
                     </div>
 
                     <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
                         <div className='grid'>
                             <label>Email : </label>
                             <div className='bg-slate-200 p-2'>
-                                <input 
-                                type='email' 
-                                placeholder='Enter Email' 
-                                name='email' value={data.email} 
-                                onChange={handleOnChange} 
-                                className='w-full h-full outline-none bg-transparent'
+                                <input
+                                    type='email'
+                                    placeholder='Enter Email'
+                                    name='email' value={data.email}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'
                                 />
                             </div>
                         </div>
                         <div>
                             <label>Password : </label>
                             <div className='bg-slate-200 p-2 flex'>
-                                <input 
-                                type={showPassword ? "text" : "password"} 
-                                name='password' value={data.password} 
-                                onChange={handleOnChange} placeholder='Enter Password' 
-                                className='w-full h-full outline-none bg-transparent' 
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    name='password' value={data.password}
+                                    onChange={handleOnChange} placeholder='Enter Password'
+                                    className='w-full h-full outline-none bg-transparent'
                                 />
                                 <div className='cursor-pointer text-xl' onClick={() => setShowPassword((prev) => !prev)}>
                                     <span>
