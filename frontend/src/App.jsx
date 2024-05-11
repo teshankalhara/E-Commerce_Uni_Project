e26118af7b34157e5f935616535993a3ece347b0
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-11 17:06:13 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { createBrowserRouter, Outlet } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import SummaryApi from "./api";
 
 
 function App() {
+  const fetchUserDetails=async()=>{
+    const dataResponse=await fetch(SummaryApi.current_user,{
+      method:SummaryApi.current_user.method,
+      credentials:"include"
+    })
+  }
+  useEffect(()=>{
+
+  },[])
+
   return (
     <>
       <ToastContainer />
