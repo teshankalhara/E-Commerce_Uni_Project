c1004212cc44e0597f668dc7653af7a0b2f132c9
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-11 17:06:28 +0530

    Create userDetails.js

---
+++ backend/controller/userDetails.js
@@ -0,0 +1,13 @@
+async function userDetailsController(req,res){
+    try{
+
+    }catch(err){
+        res.status(400).json({
+            message:err.message||err,
+            error:true,
+            success:false
+        })
+    }
+}
+
+module.exports=userDetailsController
