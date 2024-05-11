886240f0935055bac6ca03ba5629d9d84b2b5a19
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-05-11 17:06:22 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,11 @@ const router=express.Router()
 
 const userSignUpController=require('../controller/userSignUp')
 const userSignInController = require('../controller/userSignIn')
+const authToken = require('../middleware/authToken')
+const userDetailsController = require('../controller/userDetails')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
+router.get("/user-details",authToken,userDetailsController)
 
 module.exports=router
